e9774eb3516861cfad477bc35204b01c9e62e7fc
Gábor Nagy <anonym0606@gmail.com>
2021-01-27 23:46:35 +0100

    Explain the main handler logic in more detail

--- index.js
+++ index.js
@@ -52,15 +52,28 @@ client.once("ready", () => console.log("Starting..."));
 client.login(DISCORD_TOKEN).then(console.log("Logged in!"));
 
 /**
- * Listens to  message events emitted to the Client, and responds according to 
+ * Listens to  message events emitted to the Client, and responds according to
  * context.
  *
  * Generally, there are 4 cases that we handle here:
  * - A user sends a message in the specified Welcome Channel
  * - A user sends the !verify command in the specified Welcome Channel
- * - A user sends the Bot a DM with a KTH address
- * - A user sends the Bot a DM with a Token-like string
+ * - A user sends Harmony a DM with a KTH address
+ * - A user sends Harmony a DM with a Token-like string
  * Any other case is disregarded.
+ *
+ * The process is the following for any new, unverified user:
+ * 1. They join the Welcome Channel when they join the Server ('Guild' as per Discord terms).
+ * 2. They get a prompt from Harmony to use the !verify command.
+ * 3. Upon using !verify, they are sent a DM by Harmony, requesting their KTH-address.
+ * 4. The user sends their address to Harmony in the DM channel.
+ * 5. Harmony generates a verification token, and sends an email containing the token via Spam,
+ * 	  while also saving the token->email and token->discord_id association in their respective
+ * 	  key-value stores.
+ * 6. The user responds with the token in the DM channel.
+ * 7. Harmony checks that the token is valid for that user.
+ * 7.a If it is, the user is assigned the Verified role.
+ * 7.b If it is not, Harmony responds with an error message.
  *
  * TODO: Should probably break this up into smaller functions, and I am not
  * 		100% certain on how correct the async behavior is...
