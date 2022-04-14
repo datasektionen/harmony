aa42d8ff31981e4113cc699151795c697b17fc1f
Hampus Hallkvist <hampus@hallkvist.org>
2022-04-14 21:06:21 +0200

    Change Keyv to only use in memory solutions

--- src/database_config.ts
+++ src/database_config.ts
@@ -1,13 +1,9 @@
 import Keyv from "keyv";
 
 // TODO: We probably do not need 3 key-value stores for this if we used some class for this.
-const token_email = new Keyv(process.env.DB_URL, { namespace: "token_email" });
-const token_discord = new Keyv(process.env.DB_URL, {
-	namespace: "token_discord",
-});
-const verified_users = new Keyv(process.env.DB_URL, {
-	namespace: "verified_users",
-});
+const token_email = new Keyv();
+const token_discord = new Keyv();
+const verified_users = new Keyv();
 
 token_discord.clear();
 token_email.clear();
