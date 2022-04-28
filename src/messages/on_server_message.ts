4309e6f76b99ba95989966d6525b69e8d11f9d77
Hampus Hallkvist <hampus@hallkvist.org>
2022-04-28 20:45:25 +0200

    Rename on_welcome file

--- src/messages/on_server_message.ts
+++ src/messages/on_server_message.ts
@@ -1,11 +1,14 @@
 import { Message } from "discord.js";
+import { kickUnverifiedUsers } from "../commands/kick_unverified_users";
 
 export function onWelcome(message: Message, messageText: string) {
 	if (messageText === "!verify") {
 		return respondToAuthor(
 			message,
-			"Svara med din KTH-mejladress för att få en registreringskod!"
+			"Svara med din KTH-mejladress för att få en registreringskod! (ex: hello@kth.se)"
 		);
+	} else if (messageText === "!kick_unverified") {
+		return kickUnverifiedUsers(message);
 	}
 	return sendMessage(
 		message,
