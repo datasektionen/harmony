015bdf3307df1672d681ab8603b5a3ddc3d09b12
Axel Elmarsson <axel.elmarsson@gmail.com>
2022-08-16 13:12:06 +0200

    fix: push to main, not master, on medusa

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Setup the SSH environment
+mkdir -p ~/.ssh
+eval `ssh-agent -s`
+ssh-add - <<< "$SSH_PRIVATE_KEY"
+ssh-keyscan medusa.datasektionen.se >> ~/.ssh/known_hosts
+
+GIT_COMMAND="git push dokku@medusa.datasektionen.se:harmony master:main"
+
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 22" $GIT_COMMAND
