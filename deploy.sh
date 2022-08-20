5ea976dd1a7bd4a63c680e122bbc1d846ebb5692
Niklas Vatn <niklas@vatn.se>
2022-08-20 15:36:41 +0200

    fix(ci): push from main to Medusa
    I just changed the default branch name from master to main.
    This commit should fix the deploy script.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ eval `ssh-agent -s`
 ssh-add - <<< "$SSH_PRIVATE_KEY"
 ssh-keyscan medusa.datasektionen.se >> ~/.ssh/known_hosts
 
-GIT_COMMAND="git push dokku@medusa.datasektionen.se:harmony master:main"
+GIT_COMMAND="git push dokku@medusa.datasektionen.se:harmony main:main"
 
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 22" $GIT_COMMAND
