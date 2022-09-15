b84f824d447fddcc0c8ad48fb8f089d0d20715a4
Hampus Hallkvist <hampus@hallkvist.org>
2022-09-15 20:52:36 +0200

    YML -> JSON

--- src/shared/utils/read-alias-mappings.ts
+++ src/shared/utils/read-alias-mappings.ts
@@ -1,19 +1,9 @@
-import jsYml from "js-yaml";
-
-import { readFileSync } from "fs";
-
-type AlisMappings = {
-	[key: string]: string[];
-};
+import { mappings } from "../alias-mappings";
 
 export const aliasExists = (alias: string): boolean => {
-	return readAliasMappings()[alias] !== undefined;
+	return mappings[alias] !== undefined;
 };
 
 export const getAliasChannels = (alias: string): string[] => {
-	return readAliasMappings()[alias];
-};
-
-export const readAliasMappings = (): AlisMappings => {
-	return jsYml.load(readFileSync("alias-mappings.yml", "utf8")) as AlisMappings;
+	return mappings[alias];
 };
