66f508c188b9bb17ee1326b938c92b7d0e530f9f
Hampus Hallkvist <hampus@hallkvist.org>
2022-10-22 18:00:07 +0200

    Remove comment

--- src/commands/courses/courses.command.ts
+++ src/commands/courses/courses.command.ts
@@ -1,6 +1,5 @@
 import { SlashCommandBuilder } from "discord.js";
 import { CommandNames } from "../commands.names";
-//import { CoursesVariables } from "./courses.variables";
 
 export const coursesCommand = new SlashCommandBuilder()
 	.setName(CommandNames.COURSES)
