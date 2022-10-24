96b3a252be06e6de0b74ebbfab936b28fe02c001
Carlltz <carl.liljencrantz@gmail.com>
2022-10-24 21:04:11 +0200

    Updated based on comments

--- src/commands/handle-commands.ts
+++ src/commands/handle-commands.ts
@@ -17,8 +17,11 @@ export const handleCommands = (env: Env): void => {
 		}
 		try {
 			const { user, guild } = interaction;
+			if (!guild) {
+				throw new Error("Couldn't get guild from interaction!");
+			}
 			// Checks which commands the user should have access to:
-			if (guild && (await hasRoleVerified(user, guild))) {
+			if (await hasRoleVerified(user, guild)) {
 				switch (interaction.commandName) {
 					case CommandNames.PING:
 						await handlePing(interaction);
@@ -30,7 +33,6 @@ export const handleCommands = (env: Env): void => {
 						await handleKick(interaction);
 						break;
 					case CommandNames.VERIFY:
-						// Since I see no need to verify twice why not just return this instead:
 						await interaction.reply({
 							content: "You are already verified!",
 							ephemeral: true,
@@ -48,7 +50,7 @@ export const handleCommands = (env: Env): void => {
 					default:
 						throw new CommandNotFoundError(interaction.commandName);
 				}
-			} else if (guild) {
+			} else {
 				const validCommands = Object.values(CommandNames) as string[]; // Get all valid command names
 				if (interaction.commandName === CommandNames.VERIFY) {
 					await handleVerify(interaction);
@@ -56,15 +58,12 @@ export const handleCommands = (env: Env): void => {
 				} else if (validCommands.includes(interaction.commandName)) {
 					await interaction.reply({
 						content:
-							"Permission denied!\nYou first need to verify yourself using the \"/verify begin\" command.",
+							"Permission denied!\nYou first need to verify yourself using the '/verify begin' command.",
 						ephemeral: true,
 					});
 				} else {
 					throw new CommandNotFoundError(interaction.commandName);
 				}
-			} else {
-				// Good for devs to see where the error is instead of allways throwing "Permission denied!..." in else if above
-				throw new Error("Couldn't get guild from interaction!");
 			}
 		} catch (error) {
 			console.warn(error);
