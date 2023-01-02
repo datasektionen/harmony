74d41059ea316f76ea1a8a1e9e2ea67abffa7e28
Hampus Hallkvist <hampus@hallkvist.org>
2023-01-02 15:51:06 +0100

    refactor: Switch case

--- src/shared/utils/alias_to_year_mapper.ts
+++ src/shared/utils/alias_to_year_mapper.ts
@@ -3,16 +3,10 @@ import { AliasName } from "../alias-mappings";
 export function mapYearToAlias(courseYear: number): AliasName {
 	const userYear = 2000 + courseYear;
 	const year = new Date().getFullYear() - (new Date().getMonth() < 7 ? 1 : 0);
+	const currentYear = year - userYear;
 
-	const current = year - userYear;
-
-	switch (current) {
-		case 0:
-			return AliasName.YEAR1;
-		case 1:
-			return AliasName.YEAR2;
-		case 2:
-			return AliasName.YEAR3;
-	}
-	return AliasName.CS_MASTER;
+	return (
+		[AliasName.YEAR1, AliasName.YEAR2, AliasName.YEAR3][currentYear] ??
+		AliasName.CS_MASTER
+	);
 }
