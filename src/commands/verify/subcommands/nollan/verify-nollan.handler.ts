8b0910775c658d25bcd4f980471b0709f865efc0
Benjaneb <benjaneb1@gmail.com>
2023-05-25 18:48:51 +0200

    Defer reply and edit reply

--- src/commands/verify/subcommands/nollan/verify-nollan.handler.ts
+++ src/commands/verify/subcommands/nollan/verify-nollan.handler.ts
@@ -36,6 +36,8 @@ export const handleVerifyNollan = async (
 	}
 
 	try {
+		await interaction.deferReply({ ephemeral: true });
+
 		await setRoleVerified(user, interaction.guild);
 		
 		const year = new Date().getFullYear();
@@ -46,9 +48,14 @@ export const handleVerifyNollan = async (
 		// Join all pre-NG courses
 		// sf0003, sf1671, dd1337, da1600
 		const courseCodes = ["sf0003", "sf1671", "dd1337", "da1600"]
-		courseCodes.forEach(code => {
-			handleChannel(code, interaction, joinChannel, true);
-		});
+		
+		Promise.all(courseCodes.map(async code => {
+			try {
+				await handleChannel(code, interaction, joinChannel, true);
+			} catch {
+				console.log("Couldn't join channel: " + code);
+			}
+		}));
 
 		// Add all ping roles
 		await setPingRoles(user, interaction.guild);
@@ -56,11 +63,7 @@ export const handleVerifyNollan = async (
 		// Add n0llegrupps roles
 		await setNollegruppRoles(user, code, interaction.guild);
 
-		await interaction.reply({
-			content:
-				"You are now verified! You have been added to all course channels of your current year. \nYou can join or leave course channels with the `/join` and `/leave` command. \nFor more info, see: <#1020725853157593219>",
-			ephemeral: true,
-		});
+		await interaction.editReply({ content: "You are now verified! You have been added to all course channels of your current year. \nYou can join or leave course channels with the `/join` and `/leave` command. \nFor more info, see: <#1020725853157593219>" });
 	} catch (error) {
 		console.warn(error);
 		await interaction.reply({
