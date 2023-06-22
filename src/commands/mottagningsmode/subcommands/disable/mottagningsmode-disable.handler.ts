4b3b5e915ff067e895da906d8076245ba896364b
nylanderdev <rasmus.s.nylander@gmail.com>
2023-06-22 20:45:28 +0200

    Reduce code duplication in mottagnings-mode subcommands

--- src/commands/mottagningsmode/subcommands/disable/mottagningsmode-disable.handler.ts
+++ src/commands/mottagningsmode/subcommands/disable/mottagningsmode-disable.handler.ts
@@ -1,23 +1,8 @@
 import { GuildChatInputCommandInteraction } from "../../../../shared/types/GuildChatInputCommandType";
-import { isMottagningsModeActive } from "../../../../shared/utils/state";
-import { toggleMottagningsmode } from "../common";
+import { handleMottagningsmodeSet } from "../common";
 
 export const handleMottagningsmodeDisable = async (
     interaction: GuildChatInputCommandInteraction
 ): Promise<void> => {
-    await interaction.deferReply();
-    const mottagningOn = await isMottagningsModeActive(); // Read current mode
-    const newMode = "default";
-
-    if (mottagningOn) {
-        toggleMottagningsmode();
-        await interaction.editReply({
-            content: `Harmony is now in mode: ${newMode}.`
-        });
-    } else {
-        await interaction.editReply({
-            content: "Mottagnings-mode is already disabled."
-        });
-    }
-    return;
+    await handleMottagningsmodeSet(interaction, false);
 };
--- src/commands/mottagningsmode/subcommands/enable/mottagningsmode-enable.handler.ts
+++ src/commands/mottagningsmode/subcommands/enable/mottagningsmode-enable.handler.ts
@@ -1,24 +1,8 @@
 import { GuildChatInputCommandInteraction } from "../../../../shared/types/GuildChatInputCommandType";
-import { isMottagningsModeActive } from "../../../../shared/utils/state";
-import { toggleMottagningsmode } from "../common";
+import { handleMottagningsmodeSet } from "../common";
 
 export const handleMottagningsmodeEnable = async (
     interaction: GuildChatInputCommandInteraction
 ): Promise<void> => {
-    await interaction.deferReply();
-    const mottagningOn = await isMottagningsModeActive(); // Read current mode
-    const newMode = "mottagning";
-
-    if (!mottagningOn) {
-        toggleMottagningsmode();
-        await interaction.editReply({
-            content: `Harmony is now in mode: ${newMode}.`
-        });
-    } else {
-        await interaction.editReply({
-            content: "Mottagnings-mode is already enabled."
-        });
-    }
-   
-    return;
+    await handleMottagningsmodeSet(interaction, true);
 };
