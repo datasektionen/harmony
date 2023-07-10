5bac5fcdf9017e0f73a9da4353cb263051ca5da8
nylanderdev <rasmus.s.nylander@gmail.com>
2023-07-10 16:48:15 +0200

    Enable variable path import of state

--- src/shared/utils/state.ts
+++ src/shared/utils/state.ts
@@ -1,5 +1,8 @@
+const STATE_IMPORT_PATH = "../../../" + (process.env.STATE_PATH ?? "src/shared/assets/state.json")
+const STATE_WRITE_PATH = "./" + (process.env.STATE_PATH ?? "src/shared/assets/state.json")
+
 import { writeFile } from "fs/promises";
-import stateData from "../../shared/assets/state.json"
+const stateData = require(STATE_IMPORT_PATH) // eslint-disable-line @typescript-eslint/no-var-requires
 
 // Get state/mode Harmony is in (default or mottagnings-mode)
 export function getState(): string {
@@ -11,7 +14,7 @@ export async function setState(mode: string): Promise<void> {
     stateData.state = mode;
     const newStateData = JSON.stringify(stateData);
     try {
-        await writeFile("./src/shared/assets/state.json", newStateData);
+        await writeFile(STATE_WRITE_PATH, newStateData);
         console.log(`Mode successfully set to ${mode}.`);
     } catch (err) {
         console.warn(err);
