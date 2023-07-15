9a05326fa929c89d3d3443bb6fb319e3fdff86d4
Mathias Magnusson <mathias@magnusson.space>
2023-07-15 13:36:05 +0200

    Use zod to validate hodis output

--- src/shared/utils/hodis.ts
+++ src/shared/utils/hodis.ts
@@ -1,15 +1,20 @@
-type HodisUser = {
-	ugKthid: string;
-	uid: string;
-	cn: string;
-	mail: string;
-	givenName: string;
-	displayName: string;
-	year: number;
-	tag: string;
-};
+import { z } from "zod";
+
+const hodisUser = z.object({
+	ugKthid: z.string(),
+	uid: z.string(),
+	cn: z.string(),
+	mail: z.string(),
+	givenName: z.string(),
+	displayName: z.string(),
+	year: z.number(),
+	tag: z.string(),
+});
+
+type HodisUser = z.infer<typeof hodisUser>;
 
 export async function getHodisUser(kthId: string): Promise<HodisUser> {
 	const res = await fetch(`https://hodis.datasektionen.se/uid/${kthId.toLowerCase()}`);
-	return (await res.json()) as HodisUser;
+	const json = await res.json();
+	return hodisUser.parse(json);
 }
