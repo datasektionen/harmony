e2a7a2628e024eb1afe122d33424947b38faf52d
Benjaneb <benjaneb@proton.me>
2023-11-30 19:42:35 +0100

    REMOVEEEEEE

--- src/commands/register-commands.ts
+++ src/commands/register-commands.ts
@@ -1,12 +1,8 @@
-import { ApplicationCommandManager, GuildApplicationCommandManager } from "discord.js";
 import { harmonyClient, harmonyLightClient } from "..";
 import { getLightBotCommands, getOfficialBotCommands } from "./commands";
 
 export const registerCommands = async (): Promise<void> => {
 	if (process.env.DISCORD_BOT_TOKEN) {
-		// Ensures there are no application commands left from light runs
-		// await clearAppCommands(harmonyClient.application?.commands);
-
 		await Promise.all(
 			getOfficialBotCommands().map((command) =>
 				harmonyClient.application?.commands?.create(command)
@@ -14,9 +10,6 @@ export const registerCommands = async (): Promise<void> => {
 		);
 	}
 	if (process.env.DISCORD_LIGHT_BOT_TOKEN) {
-		// Ensures there are no guild commands left from non-light runs
-		// await clearAppCommands(harmonyLightClient.application?.commands);
-
 		await Promise.all(
 			getLightBotCommands().map((command) =>
 				harmonyLightClient.application?.commands?.create(command)
@@ -24,23 +17,3 @@ export const registerCommands = async (): Promise<void> => {
 		);
 	}
 };
-
-// For emergency use, i.e. if someone puts dev guild commands into production
-async function clearGuildCommands(commands?: GuildApplicationCommandManager): Promise<void> { // eslint-disable-line @typescript-eslint/no-unused-vars
-	if (!commands) {
-		return;
-	}
-	for (const [, command] of await commands.fetch()) {
-		commands.delete(command)
-	}
-}
-
-// For use if someone puts prod application commands into development
-async function clearAppCommands(commands?: ApplicationCommandManager): Promise<void> { // eslint-disable-line @typescript-eslint/no-unused-vars
-	if (!commands) {
-		return;
-	}
-	for (const [, command] of await commands.fetch()) {
-		commands.delete(command)
-	}
-}
