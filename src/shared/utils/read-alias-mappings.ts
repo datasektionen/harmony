0a9e2126bf721635394c7586a454517d942c2723
Benjaneb <benjaneb@proton.me>
2024-03-21 20:25:30 +0100

    Working all-electives alias & remove prosam in year1-3

--- src/shared/utils/read-alias-mappings.ts
+++ src/shared/utils/read-alias-mappings.ts
@@ -7,19 +7,24 @@ export const aliasExists = (alias: AliasName): boolean => {
 };
 
 export const getAliasChannels = async (guild: Guild, alias: AliasName): Promise<Collection<string, CourseChannel>> => {
+	// Logic for electives alias
 	if (alias === AliasName.ALL_ELECTIVES) {
 		const mandatories = await getAliasChannels(guild, AliasName.ALL_MANDATORY);
-		const elective_categories = ["hello"];
-		return (await getAllCourseChannels(guild))
+
+		const elective_categories = ["elective courses", "master courses"];
+
+		const elective_channels = (await getAllCourseChannels(guild))
 			.difference(mandatories)
 			.filter(channel => 
 				elective_categories.some(category => 
-					channel.parent && channel.parent.name.includes(category)
+					channel.parent && // Not null
+					channel.parent.name.toLowerCase().includes(category)
 				)
 			);
+		return elective_channels;
 	}
 
-	let channelNames = mappings[alias];
+	const channelNames = mappings[alias];
 
 	const channelNamesSet = new Set(channelNames.map((current) => current.toLowerCase()));
 
