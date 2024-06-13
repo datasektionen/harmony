cc261eac80f17e542a24f4d3749e854881691f8e
Benjaneb <71073919+Benjaneb@users.noreply.github.com>
2024-06-13 21:13:13 +0000

    Remove Content-Type

--- src/shared/utils/darkmode.ts
+++ src/shared/utils/darkmode.ts
@@ -1,9 +1,5 @@
 export const isDarkmode = async (): Promise<boolean> => {
-	const response = await fetch("https://darkmode.datasektionen.se/", {
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
+	const response = await fetch("https://darkmode.datasektionen.se/");
 	if (!response.ok)
 		return true;
 	const darkmode = await response.json();
