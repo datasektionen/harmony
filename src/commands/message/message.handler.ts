1d60533dca48d20d326a2492b83f8f21c923e370
Benjaneb <benjaneb@proton.me>
2024-06-30 00:10:00 +0200

    Implement sending attachments

--- src/commands/message/message.handler.ts
+++ src/commands/message/message.handler.ts
@@ -8,25 +8,12 @@ export const handleMessage = async (
 	const role = options.getRole("role", true);
 	const messageid = options.getString("messageid", false);
 
-	interaction.deferReply({ ephemeral: true });
+	await interaction.deferReply({ ephemeral: true });
 
-	let text = "";
-	try {
-		const message = await interaction.channel?.messages.fetch(messageid as Snowflake);
-		if (!message) {
-			interaction.editReply("A message with that ID does not exist in this channel");
-			return;
-		}
-		text = message.content;
-	} catch {
-		interaction.editReply("A message with that ID does not exist in this channel");
-		return;
-	}
-	
 	await interaction.guild.members.fetch();
 	
 	const targetMembers = interaction.guild.members.cache.filter(
-		(member) =>
+		member =>
 			member.roles.cache.some(r => r.name === role.name) && !member.user.bot
 	);
 
@@ -35,13 +22,24 @@ export const handleMessage = async (
 		return;
 	}
 
-	await Promise.allSettled(
-		targetMembers.map(member =>
-			member
-				.createDM(true)
-				.then(dm => dm.send(text))
-		)
-	);
+	const message = await interaction.channel?.messages.fetch(messageid as Snowflake)
+		.then(message => {
+			const content = message.content;
+			const files = Array.from(message.attachments.values());
+			Promise.allSettled(
+				targetMembers.map(member =>
+					member
+						.createDM(true)
+						.then(dm => dm.send({ content, files }))
+				)
+			);
+			return message;
+		})
+		.catch(() => undefined);
+	if (!message) {
+		interaction.editReply("A message with that ID does not exist in this channel");
+		return;
+	}
 
 	await interaction.editReply(
 		`Messaged all with role \`${role.name}\` (${targetMembers.size} members)`
