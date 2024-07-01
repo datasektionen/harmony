c7757d1cb571dd05471ce0a54b51b58744ecde85
Benjaneb <71073919+Benjaneb@users.noreply.github.com>
2024-07-01 20:28:44 +0000

    Update src/commands/message/message.handler.ts
    Co-authored-by: Mathias Magnusson <mathias@magnusson.space>

--- src/commands/message/message.handler.ts
+++ src/commands/message/message.handler.ts
@@ -21,7 +21,6 @@ export const handleMessage = async (
 	}
 
 	const message = await interaction.channel?.messages.fetch(messageid as Snowflake)
-		.then(message => message) // An error can either result in undefined or an exception
 		.catch(() => undefined);
 	if (!message) {
 		interaction.editReply("A message with that ID does not exist in this channel");
