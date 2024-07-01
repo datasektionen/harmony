50ce179febde1baa059e51e5e1767742430ef62d
Benjaneb <benjaneb@proton.me>
2024-07-01 20:30:35 +0200

    Simpler member fetch & await DMs

--- src/commands/message/message.handler.ts
+++ src/commands/message/message.handler.ts
@@ -1,21 +1,18 @@
-import { Snowflake } from "discord.js";
+import { Role, Snowflake } from "discord.js";
 import { GuildChatInputCommandInteraction } from "../../shared/types/GuildChatInputCommandType";
 
 export const handleMessage = async (
 	interaction: GuildChatInputCommandInteraction
 ): Promise<void> => {
 	const { options } = interaction;
-	const role = options.getRole("role", true);
-	const messageid = options.getString("messageid", false);
+	const role = options.getRole("role", true) as Role;
+	const messageid = options.getString("messageid", true);
 
 	await interaction.deferReply({ ephemeral: true });
 
 	await interaction.guild.members.fetch();
 	
-	const targetMembers = interaction.guild.members.cache.filter(
-		member =>
-			member.roles.cache.some(r => r.name === role.name) && !member.user.bot
-	);
+	const targetMembers = role.members;
 
 	if (targetMembers.size === 0) {
 		await interaction.editReply("No users with that role found");
@@ -23,23 +20,22 @@ export const handleMessage = async (
 	}
 
 	const message = await interaction.channel?.messages.fetch(messageid as Snowflake)
-		.then(message => {
-			const content = message.content;
-			const files = Array.from(message.attachments.values());
-			Promise.allSettled(
-				targetMembers.map(member =>
-					member
-						.createDM(true)
-						.then(dm => dm.send({ content, files }))
-				)
-			);
-			return message;
-		})
+		.then(message => message) // An error can either result in undefined or an exception
 		.catch(() => undefined);
 	if (!message) {
 		interaction.editReply("A message with that ID does not exist in this channel");
 		return;
 	}
+
+	const content = message.content;
+	const files = Array.from(message.attachments.values());
+	await Promise.allSettled(
+		targetMembers.map(member =>
+			member
+				.createDM(true)
+				.then(dm => dm.send({ content, files }))
+		)
+	);
 
 	await interaction.editReply(
 		`Messaged all with role \`${role.name}\` (${targetMembers.size} members)`
