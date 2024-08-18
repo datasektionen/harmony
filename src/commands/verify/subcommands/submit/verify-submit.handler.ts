8109825649a3ed293f09e1814eaacb0cbfa0eb2f
Tim Vilgot Mikael Fredenberg <vilgotf@pm.me>
2024-08-18 17:05:30 +0200

    feat: correct light verified reply

--- src/commands/verify/subcommands/submit/verify-submit.handler.ts
+++ src/commands/verify/subcommands/submit/verify-submit.handler.ts
@@ -1,6 +1,7 @@
 import { tokenUser } from "../../../../database-config";
 import * as db from "../../../../db/db";
 import { GuildChatInputCommandInteraction } from "../../../../shared/types/GuildChatInputCommandType";
+import { clientIsLight } from "../../../../shared/types/light-client";
 import { VerifyingUser } from "../../../../shared/types/VerifyingUser";
 import { setIntisRoles, setPingRoles, setRoleVerified } from "../../../../shared/utils/roles";
 import { messageIsToken, verifyUser } from "../util";
@@ -52,8 +53,10 @@ export const handleVerifySubmit = async (
 	
 	await tokenUser.delete(token); // Remove temporary token-user pair
 
+	const content = clientIsLight(interaction.client)
+		? "You are now verified!"
+		: "You are now verified! You have been added to all course channels of your current year. \nYou can join or leave course channels with the `/join` and `/leave` command. \nFor more info, see: <#1020725853157593219>";
 	await interaction.editReply({
-		content:
-			"You are now verified! You have been added to all course channels of your current year. \nYou can join or leave course channels with the `/join` and `/leave` command. \nFor more info, see: <#1020725853157593219>"
+		content: content,
 	});
 };
