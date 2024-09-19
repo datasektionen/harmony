40c12e4b2dae35cb676ce9036354a5fa5882dc51
Benjaneb <benjaneb@proton.me>
2024-09-19 18:10:59 +0200

    Add return type on sleep function

--- src/shared/utils/channel-utils.ts
+++ src/shared/utils/channel-utils.ts
@@ -216,4 +216,4 @@ export async function getAllCourseChannels(
 	return courseChannels;
 }
 
-export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+export const sleep = (ms: number): Promise<NodeJS.Timeout> => new Promise((r) => setTimeout(r, ms));
