b8d5e7e7b82dfbfa18720726955ec2fe2eae1a8c
alfredfl <alfredfl@kth.se>
2024-12-08 23:44:27 +0100

    Yet again the damn Prettier check hates me -_-

--- src/commands/kthid/kthid.handler.ts
+++ src/commands/kthid/kthid.handler.ts
@@ -8,7 +8,7 @@ export const handleKthId = async (
 	const { options } = interaction;
 	const user = options.getUser(KthIdVariables.USER, true);
 	await interaction.deferReply({ ephemeral: true });
-    const kthId = await getKthIdByUserId(user.id);
+	const kthId = await getKthIdByUserId(user.id);
 	if (!kthId) {
 		await interaction.editReply({
 			content: "Found no KTH ID mathing the provided Discord account",
