51a82065d2f07dba8da5311c39e7db43bfe42aa8
alfredfl <alfredfl@kth.se>
2024-12-12 18:04:22 +0100

    Attempt number 2 to make the Prettier check satisfied...

--- src/commands/club/subcommands/list/club-list.handler.ts
+++ src/commands/club/subcommands/list/club-list.handler.ts
@@ -1,21 +1,21 @@
 import { GuildChatInputCommandInteraction } from "../../../../shared/types/GuildChatInputCommandType";
 
 export const handleClubList = async (
-    interaction: GuildChatInputCommandInteraction,
-    roleName: string
+	interaction: GuildChatInputCommandInteraction,
+	roleName: string
 ): Promise<void> => {
-    const { guild } = interaction;
-    await guild.members.fetch();
-    const role = guild.roles.cache.find((role) => role.name === roleName);
-    const list = role?.members.map(m => m.user.tag).join(", ");
+	const { guild } = interaction;
+	await guild.members.fetch();
+	const role = guild.roles.cache.find((role) => role.name === roleName);
+	const list = role?.members.map((m) => m.user.tag).join(", ");
 
-    if (list === "") {
-        await interaction.editReply({
-            content: `There is no user with the role ${role}!`,
-        });
-    } else {
-        await interaction.editReply({
-            content: `The users with the role ${role} are: ${list}`,
-        });
-    };
+	if (list === "") {
+		await interaction.editReply({
+			content: `There is no user with the role ${role}!`,
+		});
+	} else {
+		await interaction.editReply({
+			content: `The users with the role ${role} are: ${list}`,
+		});
+	}
 };
