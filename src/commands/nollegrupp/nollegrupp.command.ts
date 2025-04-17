1c0d1b72a81039a44ed2ce9d9e22525d1b902e63
Muhammed Reza Mehdi Hamid Muhammed (Linux Mint on IdeaPad 1i) <muhammed.reza.mehdi@gmail.com>
2025-04-17 18:28:54 +0200

    Added options for the nollegrupp slash-command.

--- src/commands/nollegrupp/nollegrupp.command.ts
+++ src/commands/nollegrupp/nollegrupp.command.ts
@@ -1,6 +1,8 @@
 import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
 import { CommandNames } from "../commands.names";
 import { NollegruppSubcommands } from "./nollegrupp-subcommands.names";
+import { NollegruppAddVariables } from "./subcommands/add/nollegrupp-add.variables";
+import { NollegruppRemoveVariables } from "./subcommands/remove/nollegrupp-remove.variables";
 
 const command = new SlashCommandBuilder()
     .setName(CommandNames.NOLLEGRUPP)
@@ -12,6 +14,18 @@ command.addSubcommand((subCommand) =>
         .setName(NollegruppSubcommands.ADD)
         .setDescription(
             "Add a new nØllegrupp"
+        )
+        .addStringOption((option) => 
+            option
+                .setName(NollegruppAddVariables.NAME)
+                .setDescription("The name of the nØllegrupp")
+                .setRequired(true)
+        )
+        .addStringOption((option) => 
+            option
+                .setName(NollegruppAddVariables.CODE)
+                .setDescription("The nØllegrupp's verification code")
+                .setRequired(true)
         )
 );
 
@@ -25,6 +39,12 @@ command.addSubcommand((subCommand) =>
     subCommand
         .setName(NollegruppSubcommands.REMOVE)
         .setDescription("Remove a nØllegrupp")
+        .addStringOption((option) =>
+            option
+                .setName(NollegruppRemoveVariables.NAME)
+                .setDescription("The nØllegrupp's name")
+                .setRequired(true)
+        )
 );
 
 export const nollegruppCommand = command;
---
+++ src/commands/nollegrupp/subcommands/add/nollegrupp-add.handler.ts
@@ -0,0 +1,8 @@
+import { GuildChatInputCommandInteraction } from "../../../../shared/types/GuildChatInputCommandType";
+import { insertNollegrupp } from "../../../../db/db";
+
+async function handleNollegruppAdd(
+    interaction: GuildChatInputCommandInteraction
+): Promise<void> {
+    
+}
