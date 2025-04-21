72b24764b3ba8c971320c6a719943669ceec611a
Muhammed Reza Mehdi Hamid Muhammed (Linux Mint on IdeaPad 1i) <muhammed.reza.mehdi@gmail.com>
2025-04-21 17:49:08 +0200

    Ran prettier

--- src/commands/nollegrupp/subcommands/clear/nollegrupp-clear.handler.ts
+++ src/commands/nollegrupp/subcommands/clear/nollegrupp-clear.handler.ts
@@ -2,11 +2,12 @@ import { clearNollegrupper } from "../../../../db/db";
 import { GuildChatInputCommandInteraction } from "../../../../shared/types/GuildChatInputCommandType";
 
 export async function handleNollegruppClear(
-    interaction: GuildChatInputCommandInteraction
+	interaction: GuildChatInputCommandInteraction
 ): Promise<void> {
-    await clearNollegrupper();
+	await clearNollegrupper();
 
-    interaction.editReply({
-        content: "Successfully cleared all nØllegrupper. Remember that this breaks verification of nØllan and international students."
-    })
+	interaction.editReply({
+		content:
+			"Successfully cleared all nØllegrupper. Remember that this breaks verification of nØllan and international students.",
+	});
 }
