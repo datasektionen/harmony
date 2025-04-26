647a1997196726ec1f4682943ba356ff158b15dc
crystopherross <crystoross@gmail.com>
2025-04-26 19:04:53 +0200

    Add permission to new manual dfunkt update command

--- src/commands/dfunktUpdate/dfunkt.command.ts
+++ src/commands/dfunktUpdate/dfunkt.command.ts
@@ -1,6 +1,9 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
 import { CommandNames } from "../commands.names";
 
 export const updateDfunktCommand = new SlashCommandBuilder()
     .setName(CommandNames.UPDATE_DFUNKT)
     .setDescription("Start the dfunkt role update routine manually.");
+
+updateDfunktCommand
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild);
