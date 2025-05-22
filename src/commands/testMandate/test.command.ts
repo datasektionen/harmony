b1348cebb42a12eff8063c2c64ed6bd3d03555db
crystopherross <93978088+crystopherross@users.noreply.github.com>
2025-05-22 18:40:30 +0200

    Update src/commands/testMandate/test.command.ts
    Co-authored-by: Rafael Oliveira <rmfseo@kth.se>

--- src/commands/testMandate/test.command.ts
+++ src/commands/testMandate/test.command.ts
@@ -1,6 +1,5 @@
 import { SlashCommandBuilder } from "discord.js";
 import { CommandNames } from "../commands.names";
-// import { TestVariables } from "./test.variables";
 
 export const testCommand = new SlashCommandBuilder()
 	.setName(CommandNames.TEST)
