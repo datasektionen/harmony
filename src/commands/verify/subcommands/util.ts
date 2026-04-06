d739b001d99a630e3503a569f66d2dd3c29537ce
Muhammed Reza Mehdi Hamid Muhammed (Linux Mint on IdeaPad 1i) <muhammed.reza.mehdi@gmail.com>
2026-04-07 01:51:27 +0200

    refactor: remove dependencies on Hodis, move helpers to auth.ts.

--- src/commands/verify/subcommands/util.ts
+++ src/commands/verify/subcommands/util.ts
@@ -1,56 +1,38 @@
-import { Guild, User } from "discord.js";
-import {
-	hasAnyYearRole,
-	hasRole,
-	removeRole,
-	setDatasektionenRole,
-	setExternRole,
-	setPingRoles,
-	setRoleVerified,
-	setRole,
-} from "../../../shared/utils/roles";
-import { extractYearFromUser } from "../../../shared/utils/hodis";
-import { mapYearToAlias } from "../../../shared/utils/alias_to_year_mapper";
-import * as log from "../../../shared/utils/log";
-import { roleAliases } from "../../../shared/alias-mappings";
+// export const messageIsToken = (messageText: string): RegExpMatchArray | null =>
+// 	messageText.match(/^[a-zA-Z0-9_-]+$/);
 
-export const isKthEmail = (messageText: string): boolean =>
-	new RegExp(/^[a-zA-Z0-9]+@kth[.]se$/).test(messageText);
+// export const verifyUser = async (
+// 	user: User,
+// 	guild: Guild,
+// 	kthId: string,
+// 	isLight: boolean
+// ): Promise<void> => {
+// 	log.info(
+// 		`Verified user by kth email. kthid="${kthId}" user.id="${user.id}" user.username="${user.username}"`
+// 	);
 
-export const messageIsToken = (messageText: string): RegExpMatchArray | null =>
-	messageText.match(/^[a-zA-Z0-9_-]+$/);
+// 	await setRoleVerified(user, guild);
 
-export const verifyUser = async (
-	user: User,
-	guild: Guild,
-	kthId: string,
-	isLight: boolean
-): Promise<void> => {
-	log.info(
-		`Verified user by kth email. kthid="${kthId}" user.id="${user.id}" user.username="${user.username}"`
-	);
+// 	if (isLight) return;
 
-	await setRoleVerified(user, guild);
+// 	// const { year, yearRole } = await extractYearFromUser(kthId);
+// 	const { year, yearRole } = {year: 2024, yearRole: "D-24"}; // TODO: FIX!!
+// 	const userHasYearRole = await hasAnyYearRole(user, guild);
 
-	if (isLight) return;
+// 	if (yearRole && year) {
+// 		if (!userHasYearRole) await setRole(user, yearRole, guild);
+// 		await setDatasektionenRole(user, guild);
+// 		const alias = mapYearToAlias(year);
+// 		if (alias) {
+// 			const role = roleAliases.get(alias);
 
-	const { year, yearRole } = await extractYearFromUser(kthId);
-	const userHasYearRole = await hasAnyYearRole(user, guild);
+// 			// This should never be false.
+// 			if (role !== undefined) setRole(user, role, guild);
+// 		}
+// 	} else if (!userHasYearRole) setExternRole(user, guild);
 
-	if (yearRole && year) {
-		if (!userHasYearRole) await setRole(user, yearRole, guild);
-		await setDatasektionenRole(user, guild);
-		const alias = mapYearToAlias(year);
-		if (alias) {
-			const role = roleAliases.get(alias);
+// 	await setPingRoles(user, guild);
 
-			// This should never be false.
-			if (role !== undefined) setRole(user, role, guild);
-		}
-	} else if (!userHasYearRole) setExternRole(user, guild);
-
-	await setPingRoles(user, guild);
-
-	if (await hasRole(user, "old verified", guild))
-		await removeRole(user, "old verified", guild);
-};
+// 	if (await hasRole(user, "old verified", guild))
+// 		await removeRole(user, "old verified", guild);
+// };
