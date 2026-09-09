200f04c16c71a6144e04b4a4f4dcf4c2daa6aa22
Lind <alfredlindgren@icloud.com>
2026-09-09 13:29:49 +0200

    Small change to /kthid to make it work on n0llan

--- src/commands/kthid/kthid.handler.ts
+++ src/commands/kthid/kthid.handler.ts
@@ -1,5 +1,5 @@
 import { MessageFlags } from "discord.js";
-import { getKthIdByUserId } from "../../db/db";
+import { getKthIdByUserId, getKthIdByNolleId } from "../../db/db";
 import { GuildChatInputCommandInteraction } from "../../shared/types/GuildChatInputCommandType";
 import { KthIdVariables } from "./kthid.variables";
 
@@ -9,7 +9,13 @@ export const handleKthId = async (
 	const { options } = interaction;
 	const user = options.getUser(KthIdVariables.USER, true);
 	await interaction.deferReply({ flags: MessageFlags.Ephemeral });
-	const kthId = await getKthIdByUserId(user.id);
+
+	// The ?? is the "nullish coalescing" operator.
+	// kthId will be set to the value of getKthIdByUserId in all cases except when
+	// it returns null, in that case kthId will take the value of getKthIdByNolleId.
+	// This ensures that the command works on both regular users and on n0llan.
+	const kthId =
+		(await getKthIdByUserId(user.id)) ?? (await getKthIdByNolleId(user.id));
 	if (!kthId) {
 		await interaction.editReply({
 			content: "Found no KTH ID mathing the provided Discord account",
