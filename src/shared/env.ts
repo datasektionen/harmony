354e8dd819db324bd88d8aebe4ec9ffaad0ebaea
Muhammed Reza Mehdi Hamid Muhammed (Linux Mint on IdeaPad 1i) <muhammed.reza.mehdi@gmail.com>
2026-09-19 13:02:20 +0200

    chore: set environment variables consistently.

--- src/shared/env.ts
+++ src/shared/env.ts
@@ -1,18 +1,15 @@
-// DO NOT TOUCH THESE! THEY ARE NOT SET IN PRODUCTION
-// FOR SOME REASON AND THE "DEFAULTS" ARE THE ACTUAL
-// VALUES IN PRODUCTION! THE LOCAL TESTING PRESETS ARE
-// SET IN "compose.yaml"!
-export const DARKMODE_URL =
-	process.env.DARKMODE_URL || "https://darkmode.datasektionen.se";
-export const SPAM_URL = process.env.SPAM_URL || "https://spam.datasektionen.se";
-
-// Set default values for some environment variables
 export const NODE_ENV = process.env.NODE_ENV || "development";
+
 export const DATABASE_URL =
 	process.env.DATABASE_URL || "postgres://harmony:harmony@db/harmony";
 
-// Configure presets for local testing using nyckeln-under-dorrmattan.
+export const DARKMODE_URL =
+	process.env.DARKMODE_URL || "https://darkmode.datasektionen.se";
+
+export const SPAM_URL = process.env.SPAM_URL || "http://spam:3000";
+
 export const LDAP_PROXY_URL =
 	process.env.LDAP_PROXY_URL || "http://nyckeln:7005/user?kthid=";
+
 export const SSO_URL =
 	process.env.SSO_URL || "http://nyckeln:7003/api/users?format=single&u=";
